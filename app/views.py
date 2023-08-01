7d1af60490ad00059a31df16e1e46518435fb58f
Sidda243d <siddhusiddareddy@gmail.com>
2023-08-01 11:41:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def che(request):
+    return render(request,'che.html')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.def siddhu(request):
+def siddhu(request):
+    return render(request,'siddhu.html')
